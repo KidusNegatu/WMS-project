6a1143b91b2c5e59c1144bf9b4e40044c4ec993b
KidusNegatu <kidusNegatu19@gmail.com>
2026-08-05 15:25:18 +0300

    Update WMS_arduinoSketch.ino

--- WMS_arduinoSketch.ino
+++ WMS_arduinoSketch.ino
@@ -7,11 +7,12 @@ const int rainPin = A5;
 const int buzzerPin = 9;
 const int upBtn = 52;
 const int downBtn = 53;
-String datas[4];
-const int dataLength = 4;
+String datas[5];
+const int dataLength = 5;
 int topItem = 0;
 float temp;   
 float humidity;
+String alert;
 
 DHT dht(dhtPin, DHT11);
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
@@ -30,7 +31,6 @@ void setup() {
   delay(1000);
   digitalWrite(buzzerPin, LOW);
 };
-
 void lcdFun() {
   if (digitalRead(downBtn) == LOW && topItem < dataLength - 2) {
     topItem++;
@@ -45,6 +45,13 @@ void lcdFun() {
   lcd.print(datas[topItem]);
   lcd.setCursor(0, 1);
   lcd.print(datas[topItem + 1]);
+  if(topItem == 3) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Alert: ");
+    lcd.setCursor(0, 1);
+    lcd.print(alert);
+  };
 };
 String labels[2] = {
   "Light: ",
@@ -52,6 +59,53 @@ String labels[2] = {
 };
 String values[2];
 
+String weatherAlerts(float temper, float humi, int light, int rain){
+  if(temper < 50 && temper >= 35){
+    digitalWrite(buzzerPin, HIGH);
+    delay(5000);
+    digitalWrite(buzzerPin, LOW);
+    return "HeatWave&Drought";
+  }
+  else if(temper < 0 && temper < -20) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(5000);
+    digitalWrite(buzzerPin, LOW);
+    return "ColdWave";
+  }
+  else if(temper > 30 && temper < 45) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(5000);
+    digitalWrite(buzzerPin, LOW);
+    return "WildFires";
+  }
+  else if(temper < 0 && temper >= -3) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(5000);
+    digitalWrite(buzzerPin, LOW);
+    return "IceStorm";
+  }
+  else if(humi > 96 && humi < 100) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(5000);
+    digitalWrite(buzzerPin, LOW);
+    return "Fog";
+  }
+  else if(humi > 80 && humi < 100) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(5000);
+    digitalWrite(buzzerPin, LOW);
+    return "Flooding";
+  }
+  else if(humi > 60 && humi > 100) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(5000);
+    digitalWrite(buzzerPin, LOW);
+    return "Thunderstorms";
+  }
+  else{
+    return "None";
+  };
+};
 void loop() {
   temp = dht.readTemperature();
   humidity = dht.readHumidity();
@@ -59,6 +113,7 @@ void loop() {
   int ldrPercentage = map(ldrValue, 0, 1023, 0, 100);
   float rainValue = analogRead(rainPin);
   int rainPercentage = map(rainValue, 0, 1023, 0, 100);
+  alert = weatherAlerts(temp, humidity, ldrPercentage, rainPercentage);
   if (isnan(temp) || isnan(humidity)) {
     Serial.println("THE DHT11 IS NOT WORKING!");
     lcd.clear();
@@ -105,15 +160,17 @@ void loop() {
   datas[1] = labels[1] + values[1];
   datas[2] = "Temp: " + String(temp) + (char)223 + "C";
   datas[3] = "Humid: " + String(humidity) + "%";
+  datas[4] = alert;
   String dataAsJSON = "{"
            "\"Temp\":" + String(temp) + ","
            "\"Humidity\":" + String(humidity) + ","
            "\"LDRValue\":" + String(ldrPercentage) + ","
            "\"LDRValueADC\":" + String(ldrValue) + ","
            "\"RainValue\":" + String(rainPercentage) + "," 
-           "\"RainValueADC\":" + String(rainValue) + ","  
+           "\"RainValueADC\":" + String(rainValue) + ","
+           "\"Alert\":\"" + alert + "\"" +
        "}";
   Serial.println(dataAsJSON);
   lcdFun();
-  delay(3000);
+  delay(300);
 };
